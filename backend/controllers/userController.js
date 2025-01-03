02866469a066fe53fb1cacd65b617b36bfc35ac2
priyansh70 <priyansh70890@gmail.com>
2025-01-03 09:19:08 +0530

    Write Backend Logic for the Login and Sign Up Flow

---
+++ backend/controllers/userController.js
@@ -0,0 +1,108 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+exports.signup = async (req, res) => {
+  try {
+    // Accept user details (e.g., username, email, password) from the request body.
+    const { name, email, password } = req.body;
+
+    // Check Fields
+    if (!name || !email || !password) {
+      return res.status(400).json({ error: "All Fields are Required" });
+    }
+
+    // Validate Password
+    if (password < 8) {
+      return res
+        .status(422)
+        .json({ error: "Password should be at least 8 characters long" });
+    }
+
+    // Check User Already Registered
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    // Password Hashed
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create User
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save the new user to the database.
+    const savedUser = await user.save();
+
+    return res.status(201).json({
+      message: "User Created Successfully",
+      user: savedUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+      sucess: false,
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    // Accept login credentials (email and password) from the request body.
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: "All Fields are Required" });
+    }
+
+    //   Verify if the email exists in the database.
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "User does not exist",
+      });
+    }
+    // Use bcrypt to compare the hashed password in the database with the provided password.
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    // If the credentials are valid:
+    if (isPasswordMatch) {
+      // Generate a JWT token with a payload containing user details (e.g., user ID).
+      const payload = {
+        userId: user._id,
+        name: user.name,
+        email: user.email,
+      };
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1d", // Token expires in 1 day
+      });
+
+      const options = {
+        expiresIn: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+      // Set the JWT token as a cookie in the response.
+      return res.cookie("token", token, options).status(200).json({
+        success: true,
+        token,
+        user,
+        message: "Logged In Successfully",
+      });
+    } else {
+      return res.status(401).json({ error: "Invalid Password" });
+    }
+    // Send the token back to the client.
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+      sucess: false,
+    });
+  }
+};
